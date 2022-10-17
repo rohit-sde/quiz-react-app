b686342e01bb56fed4f15d48ce1f50ea6cb72402
Rohit Singh <rohitsinghpb4@gmail.com>
2022-10-18 03:13:54 +0530

    sync progress bar with timer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import QuestionTimer from "./QuestionTimer.jsx";
 import QUESTIONS from "../questions";
 import quizCompleteimg from "../assets/quiz-complete.png";
@@ -9,11 +9,19 @@ function Quiz() {
     const activeQuestionIndex = userAnswer.length;
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-    function handleSelectAnswer(selectedAnswer) {
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(
+        selectedAnswer
+    ) {
         setuserAnswer((prevUSerAnswers) => {
             return [...prevUSerAnswers, selectedAnswer];
         });
-    }
+    },
+    []);
+
+    const handleSkipAnswer = useCallback(
+        () => handleSelectAnswer(null),
+        [handleSelectAnswer]
+    );
 
     if (quizIsComplete) {
         return (
@@ -30,7 +38,11 @@ function Quiz() {
     return (
         <div id="quiz">
             <div id="question">
-                <QuestionTimer />
+                <QuestionTimer
+                    key={activeQuestionIndex}
+                    timeout={10000}
+                    onTimeout={handleSkipAnswer}
+                />
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {shuffledAnswers.map((answer) => (
